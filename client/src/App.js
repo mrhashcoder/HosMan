76577a1d1b1992504f4638d52862da3392f7d420
Keshav <keshavv361@gmail.com>
2020-06-03 10:28:14 +0530

    temp navbar layout added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './components/static/navbar.jsx';
+import Design from './components/static/design.jsx';
 
 class App extends React.Component {
+  
   render() {
     return(
       <div>
-        Shree Ganesh
+        <Navbar/>
+        <Design/>
       </div>
     );
   }
