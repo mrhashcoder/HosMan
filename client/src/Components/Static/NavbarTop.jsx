dc38bb5d483c0d6800c0a54b28eef2446e1d994f
mrhashcoder <abhie8101@gmail.com>
2020-07-23 21:55:10 +0530

    start again

--- client/src/Components/Static/NavbarTop.jsx
+++ client/src/Components/Static/NavbarTop.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import logo from './images/logo.png'
-import './css/NavbarTop.css'
 function NavbarTop() {
     return (
         <div>
-            <nav className="navbar navbar-expand-md bg-dark navbar-dark NavbarTop">
-                <span className="nav-item">
-                    <img src = {logo} className="logo" align="left"></img>
-                </span> 
-                <span class="navbar-nav ml-auto" align="right">
-                    <b>Developed By : Mrhashcoder & Keshav </b>
-                </span>               
-            </nav>
+
         </div>
     )
 }
